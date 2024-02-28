da01820abc134beb0e816d3ae7ce920a1851446b
Joel Jr Magno Mayol <joel.m@yotpo.com>
2024-02-28 15:04:21 +0800

    added loader script

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import React from "react";
+import { yotpoWidgetsContainer } from "react";
 import "./css/style.css";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import App from './App';
 
-window.addEventListener("load", () => {
+const initYotpoWidgets = () => {
   setTimeout(() => {
     if (typeof yotpoWidgetsContainer !== "undefined") {
       yotpoWidgetsContainer.initWidgets();
@@ -14,8 +15,11 @@ window.addEventListener("load", () => {
     } else {
       console.log("yotpoWidgetsContainer not found, no widgets to initialize");
     }
+    window.removeEventListener("load", initYotpoWidgets);
   }, 1000);
-});
+};
+
+window.addEventListener("load", initYotpoWidgets);
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
